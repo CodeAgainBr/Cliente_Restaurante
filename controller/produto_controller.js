eb009dd122fdcfd067163a31803cb13333c3817a
GilmarBec <trabalhos4.5.1.4@gmail.com>
2018-12-12 21:48:46 -0200

    add produto controller

---
+++ controller/produto_controller.js
@@ -0,0 +1,24 @@
+let carrinho = [];
+
+function produto_index(){
+	$.ajax({
+		url: "http://api-restaurante.herokuapp.com/api/v1/produtos.json",
+		async: false
+	});
+}
+
+function add_carrinho(id){
+	carrinho.push(id);
+}
+
+$(document).ajaxComplete(function(event, xhr, settings) {
+  if (settings.url === "http://api-restaurante.herokuapp.com/api/v1/produtos.json") { //Index
+    produtos = xhr.responseJSON;
+  	console.log(produtos);
+
+  	for (var i = 0; i < produtos.length; i++) {
+  		html = "<div class='produto'><img src='"+produto[i].imagem+"'><h3 id='nome'>"+produto[i].nome+"</h3><p class='descricao'>"+produto[i].descricao+"</p><span id='valor'>R$"+produto[i].valor+"</span><button onclick='add_carrinho("+produto[i].id+")'>Adicionar ao carrinho</button></div>"
+			$(".cardapio").append(html);
+		}
+  }
+});
