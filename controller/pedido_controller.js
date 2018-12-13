4f1f12f9ff3a4f50bf51a91a4522f7eb6abaf3b3
GilmarBec <trabalhos4.5.1.4@gmail.com>
2018-12-12 22:19:53 -0200

    add pedido

---
+++ controller/pedido_controller.js
@@ -0,0 +1,29 @@
+let carrinho = sessionStorage.getItem('carrinho');
+
+function pedido_index(){
+	pedido = [];
+
+	for(i in carrinho){
+		$.ajax({
+			url: "http://api-restaurante.herokuapp.com/api/v1/produtos/"+i.id+".json",
+			async: false,
+			success: function(xhr){
+				pedido.push(xhr.responseJSON);
+			}
+		});
+	}
+	
+	pedido_success();
+}
+
+function remover_carrinho(id){
+	carrinho.push(id);
+	sessionStorage.setItem('carrinho', carrinho);
+}
+
+function pedido_success(){
+	for (var i = 0; i < pedido.length; i++) {
+		html = "<div class='item'><img src='"+pedido[i].imagem+"'><h3 id='nome'>"+pedido[i].nome+"</h3><p class='descricao'>"+pedido[i].descricao+"</p><span id='valor'>R$"+pedido[i].valor+"</span><button onclick='add_carrinho("+pedido[i].id+")'>Adicionar ao carrinho</button></div>"
+		$(".pedido").append(html);
+	}
+}
